711a145e8e64aea9174778332725254a1e32144f
stealthness <stevewestinlondon@gmail.com>
2019-03-20 14:18:43 +0000

    tested getFitness function created

--- AllOnes/src/GAUtils.java
+++ AllOnes/src/GAUtils.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
 import java.util.function.Function;
 
 /**
  * Created by Stephen West on 20/03/2019.
  */
-public class GAUtils {
+class GAUtils {
 
     static Function<Individual, Double> sillyFirstGeneFitness = individual -> (double)individual.getGene(0);
 
-
     static Function<Individual, Double> sillyLastGeneFitness = individual -> (double)individual.getGene(individual.size()-1);
+
+    static Function<Individual, Double> getMeanGeneFitness = individual -> {
+        Integer s = Arrays.stream(individual.getChromosome()).sum();
+        return ((double)s)/individual.size();
+    };
 }
--- AllOnes/tests/GAUtilsTest.java
+++ AllOnes/tests/GAUtilsTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class GAUtilsTest {
 
+    private static final double TOL = 0.001;
     private int[] emptyChromosome,evenChromosome,completeChromosome;
     private Individual emptyIndividual,evenIndividual,completeIndividual;
 
@@ -29,30 +30,45 @@ class GAUtilsTest {
 
     @Test
     void testSillyFirstGeneFitnessReturnsFirstGeneOfEmptyIndividual(){
-        assertEquals(0, GAUtils.sillyFirstGeneFitness.apply(emptyIndividual));
+        assertEquals(0, GAUtils.sillyFirstGeneFitness.apply(emptyIndividual),TOL);
     }
     @Test
     void testSillyFirstGeneFitnessReturnsFirstGeneOfEvenIndividual(){
-        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(evenIndividual));
+        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(evenIndividual),TOL);
     }
     @Test
     void testSillyFirstGeneFitnessReturnsFirstGeneOfCompleteIndividual(){
-        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(completeIndividual));
+        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(completeIndividual),TOL);
     }
 
     // Test using fitness function returns the last gene
 
     @Test
     void testSillyLastGeneFitnessReturnsFirstGeneOfEmptyIndividual(){
-        assertEquals(0, GAUtils.sillyLastGeneFitness.apply(emptyIndividual));
+        assertEquals(0, GAUtils.sillyLastGeneFitness.apply(emptyIndividual),TOL);
     }
     @Test
     void testSillyLastGeneFitnessReturnsFirstGeneOfEvenIndividual(){
-        assertEquals(0, GAUtils.sillyLastGeneFitness.apply(evenIndividual));
+        assertEquals(0, GAUtils.sillyLastGeneFitness.apply(evenIndividual),TOL);
     }
     @Test
     void testSillyLastGeneFitnessReturnsFirstGeneOfCompleteIndividual(){
-        assertEquals(1, GAUtils.sillyLastGeneFitness.apply(completeIndividual));
+        assertEquals(1, GAUtils.sillyLastGeneFitness.apply(completeIndividual),TOL);
+    }
+
+    // Test using fitness function returns the mean gene fitness
+
+    @Test
+    void testGetMeanGeneFitnessReturnsFirstGeneOfEmptyIndividual(){
+        assertEquals(0.0, GAUtils.getMeanGeneFitness.apply(emptyIndividual),TOL);
+    }
+    @Test
+    void testGetMeanGeneFitnessReturnsFirstGeneOfEvenIndividual(){
+        assertEquals(0.5, GAUtils.getMeanGeneFitness.apply(evenIndividual),TOL);
+    }
+    @Test
+    void testGetMeanGeneFitnessReturnsFirstGeneOfCompleteIndividual(){
+        assertEquals(1, GAUtils.getMeanGeneFitness.apply(completeIndividual),TOL);
     }
 
 }
