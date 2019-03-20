981cf5ce4c6e1357e712e77257dbe2829d5d9eb0
stealthness <stevewestinlondon@gmail.com>
2019-03-20 14:06:35 +0000

    created tests

---
+++ AllOnes/src/GAUtils.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+
+/**
+ * Created by Stephen West on 20/03/2019.
+ */
+public class GAUtils {
+
+    static Function<Individual, Double> sillyFirstGeneFitness = individual -> -1.0;
+}
---
+++ AllOnes/tests/GAUtilsTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Stephen West on 20/03/2019.
+ */
+class GAUtilsTest {
+
+    private int[] emptyChromosome,evenChromosome,completeChromosome;
+    private Individual emptyIndividual,evenIndividual,completeIndividual;
+
+    @BeforeEach
+    void SetUp(){
+        emptyChromosome = new int[]{0,0,0,0,0,0,0,0};
+        evenChromosome = new int[]{1,1,1,0,0,0,1,0};
+        completeChromosome = new int[]{1,1,1,1,1,1,1,1};
+
+        emptyIndividual = new Individual(emptyChromosome);
+        evenIndividual = new Individual(evenChromosome);
+        completeIndividual = new Individual(completeChromosome);
+    }
+
+
+
+
+    @Test
+    void testSillyFirstGeneFitnessReturnsFirstGeneOfEmptyIndividual(){
+        assertEquals(0, GAUtils.sillyFirstGeneFitness.apply(emptyIndividual));
+    }
+    @Test
+    void testSillyFirstGeneFitnessReturnsFirstGeneOfEvenIndividual(){
+        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(evenIndividual));
+    }
+    @Test
+    void testSillyFirstGeneFitnessReturnsFirstGeneOfCompleteIndividual(){
+        assertEquals(1, GAUtils.sillyFirstGeneFitness.apply(completeIndividual));
+    }
+
+}
