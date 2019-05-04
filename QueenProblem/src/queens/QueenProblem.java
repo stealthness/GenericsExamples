4c003a9041b0706d9653984d87eb507a08b7fc9d
stealthness <stevewestinlondon@gmail.com>
2019-05-04 01:45:40 +0100

    fix bizarre last char of string issue

--- QueenProblem/src/queens/QueenProblem.java
+++ QueenProblem/src/queens/QueenProblem.java
@@ -3,6 +3,7 @@ package queens;
 import lombok.Data;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Created by Stephen West on 19/04/2019.
@@ -15,13 +16,22 @@ public class QueenProblem {
     int[] positions;
 
 
-    public QueenProblem(String string){
+    public QueenProblem(String position){
         this.positions = new int[DEFAULT_SIZE];
+        IntStream.range(0,DEFAULT_SIZE).forEach(i -> {
+            if (i < DEFAULT_SIZE-1){
+                this.positions[i] = Integer.parseInt(position.substring(i,i+1));
+            }else{
+                this.positions[i] = Integer.parseInt(position.substring(position.length()-1));
+            }
+        });
         board = new Board();
     }
 
     public QueenProblem(int[] positions){
-        this.positions = positions;
+        IntStream.range(0,DEFAULT_SIZE).forEach(i -> {
+            this.positions[i] = positions[i];
+        });
         board = new Board();
     }
 
@@ -36,5 +46,19 @@ public class QueenProblem {
         var sb = new StringBuilder();
         Arrays.stream(positions).forEach(sb::append);
         return sb.toString();
+    }
+
+    /**
+     * retruns true if the the board is valid set of queen position, that is 1 queen per row
+     * @return
+     */
+    public boolean isValid() {
+        return IntStream.range(0,DEFAULT_SIZE)
+                .allMatch(i -> {
+                    System.out.println(i + " " + this.toString());
+                    return Arrays.stream(positions)
+                            .filter(j -> i == j)
+                            .count() == 1;
+                });
     }
 }
--- QueenProblem/tests/queens/QueenProblemTest.java
+++ QueenProblem/tests/queens/QueenProblemTest.java
@@ -2,7 +2,7 @@ package queens;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
@@ -16,19 +16,36 @@ class QueenProblemTest {
     void testToStringMethod() {
 
         QueenProblem qb1 = new QueenProblem("000000000");
-        QueenProblem qb2 = new QueenProblem(new int[DEFAULT_SIZE]);
 
         assertEquals(DEFAULT_SIZE, qb1.toString().length());
+
+        qb1 = new QueenProblem("01234567");
+        assertEquals("01234567", qb1.toString());
 
     }
 
     @Test
     void getFitnessOnEmptyQueensProblem() {
         QueenProblem qb1 = new QueenProblem("000000000");
-        QueenProblem qb2 = new QueenProblem(new int[DEFAULT_SIZE]);
 
         // fitness for no queens set will be 64
         assertEquals(64, qb1.getFitness());
-        assertEquals(64, qb2.getFitness());
+    }
+
+    @Test
+    void testIsValidString(){
+        // not valid
+        QueenProblem qb1 = new QueenProblem("000000000");
+        assertFalse(qb1.isValid());
+        qb1 = new QueenProblem("1123456");
+        assertFalse(qb1.isValid());
+        qb1 = new QueenProblem("56732189");
+        assertFalse(qb1.isValid());
+
+        // valid
+        qb1 = new QueenProblem("01234567");
+        assertTrue(qb1.isValid());
+        qb1 = new QueenProblem("03721456");
+        assertTrue(qb1.isValid());
     }
 }
