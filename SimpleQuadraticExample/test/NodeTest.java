c3f09d1da4cf26da97e098aeb3a490f41424bdb9
stealthness <stevewestinlondon@gmail.com>
2019-05-15 20:53:46 +0100

    refactoring

--- SimpleQuadraticExample/test/NodeTest.java
+++ SimpleQuadraticExample/test/NodeTest.java
@@ -9,18 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class NodeTest {
 
     private static final double TOL = 0.000001;
-    BiFunction<Double,Double,Double> add = (a,b)-> a+b;
-    BiFunction<Double,Double,Double> subtract = (a,b)-> a-b;
-    BiFunction<Double,Double,Double> multiply = (a,b)-> a*b;
-    BiFunction<Double,Double,Double> protectedDivision = (a,b)-> (b==0)?1.0:a/b;
+    private BiFunction<Double,Double,Double> add = Double::sum;
+    private BiFunction<Double,Double,Double> subtract = (a, b)-> a-b;
+    private BiFunction<Double,Double,Double> multiply = (a, b)-> a*b;
+    private BiFunction<Double,Double,Double> protectedDivision = (a, b)-> (b==0)?1.0:a/b;
     private double v0;
     private double v1;
 
-    Node t0;
-    Node t1;
+    private Node t0;
+    private Node t1;
 
-    VariableNode x0;
-    VariableNode x1;
+    private VariableNode x0;
+    private VariableNode x1;
 
 
     @BeforeEach
@@ -51,22 +51,23 @@ class NodeTest {
 
     @Test
     void simpleAddLambdaTest(){
-        assertEquals(1.0, add.apply(0.4,0.6),TOL);
+        assertEquals(v0+v1, add.apply(v0,v1),TOL);
     }
 
     @Test
     void simpleSubtractLambdaTest(){
-        assertEquals(-0.2, subtract.apply(0.4,0.6),TOL);
+        assertEquals(v0-v1, subtract.apply(v0,v1),TOL);
     }
 
     @Test
     void simpleMultiplayLambdaTest(){
-        assertEquals(0.7, multiply.apply(0.5,1.4),TOL);
+        assertEquals(v0*v1, multiply.apply(v0,v1),TOL);
     }
 
     @Test
     void simpleProtectedDivisionLambdaTest(){
         assertEquals(1.0, protectedDivision.apply(3.3,0.0),TOL);
+        v1 = 0.0;
         assertEquals(1.2, protectedDivision.apply(3.6,3.0),TOL);
     }
 
