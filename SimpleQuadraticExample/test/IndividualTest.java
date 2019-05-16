aa33baac4fa085076c6c08af88559f2f992ede03
Stealthness <stevewestinlondon@gmail.com>
2019-05-16 17:29:05 +0100

    improved Individual Test

--- SimpleQuadraticExample/test/IndividualTest.java
+++ SimpleQuadraticExample/test/IndividualTest.java
@@ -42,17 +42,41 @@ class IndividualTest {
 
     @Test
     void testEvaluation(){
-        assertEquals(1.0,individual.get(new double[]{0.0}),TOL);
-        assertEquals(2.0,individual.get(new double[]{1.0}),TOL);
-        assertEquals(0.0,individual.get(new double[]{-1.0}),TOL);
     }
 
 
 
     @Test
-    void getFitness() {
-
+    void getFitnessOnExpressXEqual2() {
+        individual.setRoot(new TerminalNode(2.0));
         individual.evaluate();
+        assertEquals(2.0,individual.get(new double[]{0.0}),TOL);
+        assertEquals(2.0,individual.get(new double[]{1.0}),TOL);
+        assertEquals(2.0,individual.get(new double[]{-1.0}),TOL);
+        assertEquals(17.98, individual.getFitness(),TOL);
+    }
+
+    @Test
+    void getFitnessOnExpressXEqualXSquaredPlus1() {
+
+        Node subtree = new FunctionNode(GPUtils.multiply, "*", new VariableNode(0),new VariableNode(0)) ;
+        individual.setRoot(new FunctionNode(GPUtils.add,"+", new TerminalNode(1.0),subtree));
+        individual.evaluate();
+        assertEquals(1.0,individual.get(new double[]{0.0}),TOL);
+        assertEquals(2.0,individual.get(new double[]{1.0}),TOL);
+        assertEquals(2.0,individual.get(new double[]{-1.0}),TOL);
+        assertEquals(11.0, individual.getFitness(),TOL);
+    }
+
+    @Test
+    void testChangeRootNodeOnIndividual(){
+
+        individual.setRoot(new FunctionNode(GPUtils.add,"+", new VariableNode(0),new TerminalNode(1.0)));
+        individual.evaluate();
+
+        assertEquals(1.0,individual.get(new double[]{0.0}),TOL);
+        assertEquals(2.0,individual.get(new double[]{1.0}),TOL);
+        assertEquals(0.0,individual.get(new double[]{-1.0}),TOL);
 
         assertEquals(7.7, individual.getFitness(),TOL);
     }
