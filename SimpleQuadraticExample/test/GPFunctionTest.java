9e84e9dbf59fbcf832efc6b8919fc0840844816a
stealthness <stevewestinlondon@gmail.com>
2019-05-17 20:21:56 +0100

    test complete

--- SimpleQuadraticExample/test/GPFunctionTest.java
+++ SimpleQuadraticExample/test/GPFunctionTest.java
@@ -14,15 +14,33 @@ class GPFunctionTest {
     }
 
     @Test
-    void testCreatAddGPFunction(){
+    void testCreateAddGPFunction(){
         assertCreateGPFunction(d0+d1,"add","+",
                 new GPFunction(GPUtils.add,"add","+"));
     }
 
+    @Test
+    void testCreateSubtractFunction(){
+        assertCreateGPFunction(d0-d1,"subtract","-",
+                new GPFunction(GPUtils.subtract,"subtract","-"));
+    }
+
+    @Test
+    void testCreateMutiplyFunction(){
+        assertCreateGPFunction(d0*d1,"multiply","*",
+                new GPFunction(GPUtils.multiply,"multiply","*"));
+    }
+
+    @Test
+    void testCreateProtectedDivisionFunction(){
+        assertCreateGPFunction((d1==0)?1.0:d0/d1,"division","/",
+                new GPFunction(GPUtils.protectedDivision,"division","/"));
+    }
+
     void assertCreateGPFunction(Double expValue, String expFunctionName, String expClojureName, GPFunction function){
         assertEquals(expValue,function.apply(d0,d1));
-        assertEquals("add", function.getFunctionName());
-        assertEquals("+",function.getClojureString());
+        assertEquals(expFunctionName, function.getFunctionName());
+        assertEquals(expClojureName,function.getClojureString());
     }
 
 }
