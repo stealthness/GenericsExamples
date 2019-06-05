2a025b9ad49ce6520657ea547af37b3d5788be83
Stealthness <stevewestinlondon@gmail.com>
2019-06-05 22:12:49 +0100

    refactoring

--- SimpleQuadraticExample/test/TestUtils.java
+++ SimpleQuadraticExample/test/TestUtils.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,53 +7,53 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestUtils {
 
     private static final double TOL =  0.0001;
-    public static Double[] range1to1 = new Double[]{-1.0,1.0};
-    public static Double[] range1to2 = new Double[]{1.0,2.0};
-    public static Double[] range0to1 = new Double[]{0.0,1.0};
-    public static Node eNode = new EphemeralNode(range1to1);
+    static Double[] range1to1 = new Double[]{-1.0,1.0};
+    static Double[] range1to2 = new Double[]{1.0,2.0};
+    static Double[] range0to1 = new Double[]{0.0,1.0};
+    static Node eNode = new EphemeralNode(range1to1);
 
-    public static Node zeroNode = new TerminalNode(0.0);
-    public static Node oneNode = new TerminalNode(1.0);
-    public static Node twoNode = new TerminalNode(2.0);
-    public static Node threeNode = new TerminalNode(3.0);
-    public static Node fourNode = new TerminalNode(4.0);
-
-
-    public static Node xNode = new VariableNode(0);
-    public static Node addNode = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(oneNode,oneNode));
-    public static Node multiplyNode = new FunctionNode(new GPBiFunction(GPUtils.multiplyBiFunction),Arrays.asList(oneNode,oneNode));
+    static Node zeroNode = new TerminalNode(0.0);
+    static Node oneNode = new TerminalNode(1.0);
+    static Node twoNode = new TerminalNode(2.0);
+    static Node threeNode = new TerminalNode(3.0);
+    static Node fourNode = new TerminalNode(4.0);
 
 
-    public static Node xPlusOne = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(xNode,oneNode));
-    public static Node xPlusTwo = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(xNode,twoNode));
-    public static Node onePlusX = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(oneNode,xNode));
-    public static Node twoPlusX = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(twoNode,xNode));
+    static Node xNode = new VariableNode(0);
+    static Node addNode = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(oneNode,oneNode));
+    static Node multiplyNode = new FunctionNode(new GPBiFunction(GPUtils.multiplyBiFunction),Arrays.asList(oneNode,oneNode));
 
 
-    public static Node absOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"),Arrays.asList(oneNode));
-    public static Node recipOneNode = new FunctionNode(new GPSingleFunction(GPUtils.reciprocal,"recip"),Arrays.asList(oneNode));
+    static Node xPlusOne = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(xNode,oneNode));
+    static Node xPlusTwo = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(xNode,twoNode));
+    static Node onePlusX = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(oneNode,xNode));
+    static Node twoPlusX = new FunctionNode(new GPBiFunction(GPUtils.addBiFunction),Arrays.asList(twoNode,xNode));
 
-    public static Node addOneTwoThree = new FunctionNode(new GPMultiFunction(GPUtils.addBiFunction,"+"),Arrays.asList(oneNode,twoNode,threeNode));
 
-    static Node absabsOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"),Arrays.asList(absOneNode));
-    static Node absabsabsOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"),Arrays.asList(absabsOneNode));
+    static Node absOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"), Collections.singletonList(oneNode));
+    static Node recipOneNode = new FunctionNode(new GPSingleFunction(GPUtils.reciprocal,"recip"), Collections.singletonList(oneNode));
+
+    static Node addOneTwoThree = new FunctionNode(new GPMultiFunction(GPUtils.addBiFunction,"+"),Arrays.asList(oneNode,twoNode,threeNode));
+
+    static Node absabsOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"), Collections.singletonList(absOneNode));
+    static Node absabsabsOneNode = new FunctionNode(new GPSingleFunction(GPUtils.abs,"abs"), Collections.singletonList(absabsOneNode));
 
 
     /**
      * default range of [-1.0,1.0]
      */
-    public static Double[] createRandomInput(int size){
+    static Double[] createRandomInput(int size){
         return createRandomInput(size, range1to1);
     }
 
-    public static Double[] createRandomInput(int size, Double[] range){
+    private static Double[] createRandomInput(int size, Double[] range){
         Double[] inputs = new Double[size];
         inputs[0] = 0.67;
         IntStream.range(0,size).forEach(i -> inputs[i] = getRandomInRange(range[0],range[1]));
         return inputs;
     }
 
-    public static void assertInRange(Double[] expRange, Node actNode, int size, Double[] inputs, Double[] inputRange){
+    private static void assertInRange(Double[] expRange, Node actNode, int size, Double[] inputs, Double[] inputRange){
         Double actResult = actNode.calculate(inputs);
         assertTrue(actResult  <= expRange[1], String.format("%f is not less than %f",actResult, expRange[1]));
         assertTrue(actResult  >= expRange[0], String.format("%f is not greater than %f",actResult, expRange[0]));
@@ -63,25 +64,25 @@ class TestUtils {
         assertInRange(expRange, actNode, size, createRandomInput(size, range1to1));
     }
 
-    public static void assertInRange(Double[] expRange, Node actNode, int size, Double[] inputRange){
+    static void assertInRange(Double[] expRange, Node actNode, int size, Double[] inputRange){
         assertInRange(expRange, actNode, size, createRandomInput(size, inputRange),inputRange);
     }
 
 
 
-    public static void assertNode(Double expResult, Node actNode, Double[] inputs){
+    static void assertNode(Double expResult, Node actNode, Double[] inputs){
         Double actResult = actNode.calculate(inputs);
         assertEquals(expResult,actResult ,TOL ,String.format("\nfunction : %s\ninputs %s\n",
                 actNode.print(), Arrays.asList(inputs).toString()));
     }
 
-    public static void assertNode(Double expResult, Node actNode) {
+    static void assertNode(Double expResult, Node actNode) {
         Double actResult = actNode.calculate(createRandomInput(1));
         assertEquals(expResult, actResult, TOL, String.format("\nfunction : %s",
                 actNode.print()));
     }
 
-    public static void assertNode(Node expNode, Node actNode) {
+    static void assertNode(Node expNode, Node actNode) {
         final String msg = String.format("\nfunction : %s",actNode.print());
         assertEquals(expNode.getClass(), actNode.getClass(), msg);
         assertNodeSize(expNode.size(),expNode.getDepth(),actNode);
@@ -93,7 +94,7 @@ class TestUtils {
         }
     }
 
-    public static void assertNodeSize(int expSize, int expDepth, Node actNode){
+    static void assertNodeSize(int expSize, int expDepth, Node actNode){
         final String msg = String.format("\nfunction : %s",actNode.print());
         assertEquals(expSize, actNode.size(),msg);
         assertEquals(expDepth, actNode.getDepth(),msg);
