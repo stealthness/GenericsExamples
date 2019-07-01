a098e93008cda12f4ab88348e842e3db95f78bf4
Stealthness <stevewestinlondon@gmail.com>
2019-07-01 17:35:34 +0100

    test wpassed

--- GPExample/src/GPUtils.java
+++ GPExample/src/GPUtils.java
@@ -4,5 +4,5 @@ import java.util.function.BiFunction;
 public class GPUtils {
 
     static BiFunction<Double[], List<Node<Double[],Double>>, Double> identity = ((inputs, nodes) -> nodes.get(0).calculate(inputs));
-    static BiFunction<Double[], List<Node<Double[],Double>>, Double> abs = null; //((inputs, nodes) -> -nodes.get(0).calculate(inputs));
+    static BiFunction<Double[], List<Node<Double[],Double>>, Double> abs = ((inputs, nodes) -> Math.abs(nodes.get(0).calculate(inputs)));
 }
