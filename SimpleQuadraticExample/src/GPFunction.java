9d6291d39a69b842da1d6c2f4c2de662602eace8
Stealthness <stevewestinlondon@gmail.com>
2019-07-17 11:46:40 +0100

    add javadoc added default on - getMinSubNodes - getMaximumSubNodes

--- SimpleQuadraticExample/src/GPFunction.java
+++ SimpleQuadraticExample/src/GPFunction.java
@@ -4,8 +4,26 @@ interface GPFunction {
 
     Double apply(Double[] inputs, List<Node> nodes);
 
+    /**
+     * Returns a clojure String representation of the function
+     * @return a string representation of the function
+     */
     String toClojureString();
 
-    int getMaxSubNodes();
+    /**
+     * Returns the the maximum number of sub-nodes that a function, default is -1 indicating no maximum.
+     * @return max number of sub-nodes
+     */
+    default int getMaxSubNodes(){
+        return -1;
+    };
+
+    /**
+     * Returns the the minimum number of sub-nodes that a function, default is 1 node
+     * @return min number of sub-nodes
+     */
+    default int getMinSubNodes(){
+        return 1;
+    }
 
 }
