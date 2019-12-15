c57920f080e3134745085e3fbbaa73efd63c7eae
Stealthness <stevewestinlondon@gmail.com>
2019-12-15 22:21:56 +0000

    working other functions

--- MyGPExample/tests/TestNodesAtDepth3.java
+++ MyGPExample/tests/TestNodesAtDepth3.java
@@ -10,26 +10,42 @@ public class TestNodesAtDepth3 {
     Double[] inputs = new Double[]{1.0,2.0, -1.5};
 
     @Test
-    void testSubtract3from2(){
-        Node actNode = TestUtils.getFunctionNode("-", Arrays.asList(TestUtils.getConstantNode(2.0),
-                TestUtils.getConstantNode(3.0)));
-        TestUtils.assertNode(Optional.of("(- 2.00 3.00)"),Optional.of(2),Optional.of(3),
-                Optional.of(-1.0),Optional.of(inputs),actNode);
-
-        assertNode(getTestCase(1), actNode);
-    }
-    @Test
     void testMulti2and3(){
         Node actNode = TestUtils.getFunctionNode("*", Arrays.asList(TestUtils.getConstantNode(2.0),
                 TestUtils.getConstantNode(3.0)));
-        TestUtils.assertNode(Optional.of("(* 2.00 3.00)"),Optional.of(2),Optional.of(3),
-                Optional.of(6.0),Optional.of(inputs),actNode);
 
         assertNode(getTestCase(0), actNode);
     }
 
+    @Test
+    void testSubtract3from2(){
+        Node actNode = TestUtils.getFunctionNode("-", Arrays.asList(TestUtils.getConstantNode(2.0),
+                TestUtils.getConstantNode(3.0)));
+
+        assertNode(getTestCase(1), actNode);
+    }
+
+    @Test
+    void testAdd2and3(){
+        Node actNode = TestUtils.getFunctionNode("+", Arrays.asList(TestUtils.getConstantNode(2.0),
+                TestUtils.getConstantNode(3.0)));
+
+        assertNode(getTestCase(2), actNode);
+    }
+
+    @Test
+    void testDivide3from2(){
+        Node actNode = TestUtils.getFunctionNode("/", Arrays.asList(TestUtils.getConstantNode(2.0),
+                TestUtils.getConstantNode(3.0)));
+
+        assertNode(getTestCase(3), actNode);
+    }
+
+
+
     void assertNode(String testcase, Node actNode) {
         String[] parts = testcase.split(",");
+        Double[] inputs = Arrays.stream(parts[4].replaceAll("[\\[\\]]", "").split(";")).map(s -> Double.parseDouble(s)).toArray(Double[]::new);
         TestUtils.assertNode(Optional.of(parts[0]),Optional.of(Integer.parseInt(parts[1])),
                 Optional.of(Integer.parseInt(parts[2])), Optional.of(Double.parseDouble(parts[3])),
                 Optional.of(inputs),actNode);
@@ -44,6 +60,7 @@ public class TestNodesAtDepth3 {
     final static String TEST_CASES = """
             (* 2.00 3.00),2,3,6.0,[1.0;2.0;-1.5]
             (- 2.00 3.00),2,3,-1.0,[1.0;2.0;-1.5]
-            (+ 2.00 3.00),2,3,5.0,[1.0;2.0;-1.5]""";
+            (+ 2.00 3.00),2,3,5.0,[1.0;2.0;-1.5]
+            (/ 2.00 3.00),2,3,0.6666666666666666,[1.0;2.0;-1.5]""";
 
 }
